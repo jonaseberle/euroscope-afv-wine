5a73225ca48f6e81a1c0067cb40f3374c190c841
Jonas Eberle <flightvision@googlemail.com>
2022-01-29 19:51:59 +0100

    [FEATURE] add install script

---
+++ README.md
@@ -0,0 +1,69 @@
+# VATSIM EuroScope and AfV on wine
+This is a script that's meant to facilitate installing an ATC-environment for Linux users.
+
+If everything goes well, it does not require any additional interaction.
+
+As both the essential VATSIM ATC programs [EuroScope](https://www.euroscope.hu/) and [Audio for VATSIM](https://audio.vatsim.net/) are Windows-only (why?!), this uses `wine` as emulation layer.
+
+## Remarks
+It installs the newest EuroScope beta version. We could make that an option if users need a choice there.
+
+`wine` integrates into your host's audio system and registers all application audio channels there. Thus, just select the "default" audio device in the Windows applications and do the routing in your host. See [Wine Sound](https://wiki.winehq.org/Sound) for more information.
+
+## Requirements:
+* `bash`, `unzip`, `grep`, `wget`
+* `wine` and `winetricks`
+* There will be a warning if it misses something it needs.
+
+## Usage:
+
+```bash
+# create a new directory for your wine environment and change into it
+wineDir=$USER/VATSIM-ATC/wine
+mkdir -p "$wineDir"
+cd "$wineDir"
+
+# download the installer script
+wget https://raw.githubusercontent.com/jonaseberle/euroscope-afv-wine/main/euroscope-afv-wine_install.sh
+
+# make executable
+chmod +x euroscope-afv-wine_install.sh
+
+# and execute it
+./euroscope-afv-wine_install.sh
+
+# it does some checks and asks you for confirmation before starting the installation
+```
+
+... expect around 15min until the installation is finished. If everything worked ok, you will be able to start 
+EuroScope and Audio for VATSIM via your usual applications menu.
+
+You'll find your configured "Documents" directory (`xdg-user-dir DOCUMENTS`) labelled "Documents" in the Windows file browsers, too. This can be adjusted with `winecfg`. EuroScope expects its settings files at `"Documents"/EuroScope`.
+
+## Tested with:
+* wine 7.0-rc5
+* wine 6
+* Manjaro, Ubuntu 20.4
+* please report your successes/failures with other environments
+
+## Known issues:
+* Audio for VATSIM sometimes starts fine, then on another day it might not start any more. It probably has something to do with its internal updater. 
+* EuroScope hangs on quitting, after saving changed settings. ([WineHQ AppDB entry](https://appdb.winehq.org/objectManager.php?sClass=version&iId=32239))
+* [AFV_BRIDGE](https://github.com/AndyTWF/afv-euroscope-bridge) does not work. EuroScope message: "AFV_BRIDGE: Unable to open communication for AfV Bridge".
+* [VCH Plugin](https://github.com/DrFreas/VCH) outputs a message but seems to work fine. EuroScope message: "ERROR DATA: ERROR: Request didn't work".
+
+If you have any updates on these, please report. I would like to keep this list up-to-date.
+
+## Suggestions:
+Try updating `winetricks` if you experience problems with the "Installing libs…" step:
+```bash
+[sudo] winetricks --self-update
+```
+
+## Contact:
+Contributions of any form are very welcome. You can approach me via the "discussions" or "issues" functions here (prefered) or via my [VATSIM forums profile](https://forums.vatsim.net/profile/191848-jonas-eberle/).
+
+## Thank you
+This is built on top of the work of Samir Gebran https://forums.vatsim.net/topic/31019-euroscope-on-linux-howto/.
+
+Thank you to the creators of EuroScope and Audio for VATSIM. I think both are very able and intelligent programs. Maybe next time do something more portable.
---
+++ euroscope-afv-wine_install.sh
@@ -0,0 +1,162 @@
+#!/bin/bash
+# Installs VATSIM ATC software (EuroScope and Audio for VATSIM) into a wine environment
+# Building on top of the work of Samir Gebran https://forums.vatsim.net/topic/31019-euroscope-on-linux-howto/
+# License: MIT
+
+# saner programming env: these switches turn some bugs into errors
+set -o errexit -o pipefail -o noclobber -o nounset
+
+logFilename="euroscope-afv-wine_install.log"
+
+#black="\033[0;30m"
+#blackb="\033[1;30m"
+#white="\033[0;37m"
+#whiteb="\033[1;37m"
+red="\033[0;31m"
+#redb="\033[1;31m"
+green="\033[0;32m"
+greenb="\033[1;32m"
+#yellow="\033[0;33m"
+#yellowb="\033[1;33m"
+#blue="\033[0;34m"
+#blueb="\033[1;34m"
+#purple="\033[0;35m"
+purpleb="\033[1;35m"
+#lightblue="\033[0;36m"
+#lightblueb="\033[1;36m"
+underline="\033[4m"
+
+fEnd="\033[0m"
+fSection=$green$underline
+fHighlight=$green
+fStatus=$purpleb
+fInfo=$greenb
+fError=$red
+
+echo "You should call this program from a fresh (empty) directory where you want your EuroScope/AfV setup with wine."
+printf "  Suggestion:        %s\n" "$HOME/VATSIM-ATC/wine"
+printf "  Current directory: %s\n" "$PWD"
+printf "This program will only change files inside this directory. Anyways the built win environment has access to all files that your user has access to.\n"
+
+# check if required programs available in $PATH
+for prog in pkill pwait unzip grep wine winetricks wget; do
+    if ! type $prog > /dev/null; then
+		printf "%bERROR: we need the program ‹%s›\n%b" "${fError}" "$prog" "$fEnd"
+		exit 1
+	fi
+done
+
+wineBin="$(which wine)"
+winetricksBin="$(which winetricks)"
+
+printf "\n%bInformation about your system:%b\n" "$fInfo" "$fEnd"
+printf "  wine version      : %s\n" "$("$wineBin" --version)"
+printf "  winetricks version: %s\n" "$("$winetricksBin" --version)"
+
+printf "%b\nAre you sure you want to create or update the wine environment in the current directory?%b" "$fHighlight" "$fEnd"
+read -p " [y/n] " -n 1 -r
+echo
+
+[[ $REPLY =~ ^[Yy]$ ]] || exit 1
+
+function download() {
+	local _url="$1"
+	local _filename="$2"
+	if [ -f "$_filename" ]; then
+		printf "%bFound ‹%s› in current directory. Not downloading from ‹%s›.\n%b" "$fStatus" "$_filename" "$_url" "$fEnd"
+		return 0
+	fi
+	printf "%bDownloading ‹%s› to current directory…\n%b" "$fStatus" "$_url" "$fEnd"
+	wget "$_url" -O "$_filename"
+}
+
+function wine() {
+	printf "%bRunning \"WINEARCH=win32 WINEPREFIX=\"$PWD\" wine $*\"…\n%b" "$fStatus" "$fEnd" \
+		| tee -a "$logFilename"
+	printf "  %bFind its output in ‹%s› if you need it%b\n" "$fInfo" "$logFilename" "$fEnd"
+	WINEARCH=win32 WINEPREFIX="$PWD" "$wineBin" "$@" &>> "$logFilename"
+}
+
+function winetricks() {
+	printf "%bRunning \"WINEARCH=win32 WINEPREFIX=\"$PWD\" winetricks $*\"…\n%b" "$fStatus" "$fEnd" \
+		| tee -a "$logFilename"
+	printf "  %bFind its output in ‹%s› if you need it%b\n" "$fInfo" "$logFilename" "$fEnd"
+	WINEARCH=win32 WINEPREFIX="$PWD" "$winetricksBin" "$@" &>> "$logFilename"
+}
+
+printf "\n%bConfiguring WINEPREFIX…\n%b" "$fSection" "$fEnd"
+
+printf "\n%bShutting down other wine processes…\n%b" "$fStatus" "$fEnd"
+pkill wineserver || true
+pwait wineserver || true
+
+# This seems to circumvent the "Mono is missing" prompt
+WINEDLLOVERRIDES="mscoree=" wine wineboot
+wine winecfg -v win7
+
+printf "\n%bInstalling libs…\n%b" "$fSection" "$fEnd"
+# This seems to set the Windows version to WinXP
+winetricks --unattended dotnet40
+winetricks --unattended dotnet45
+winetricks --unattended dotnet46
+winetricks --unattended dotnet461
+winetricks --unattended dotnet462
+winetricks --unattended dotnet472
+winetricks --unattended dotnet48
+winetricks --unattended iertutil
+winetricks --unattended msls31
+winetricks --unattended msxml6
+winetricks --unattended urlmon
+winetricks --unattended vcrun2010
+winetricks --unattended vcrun2017
+winetricks --unattended wininet 
+
+printf "\n%bDownloading programs…\n%b" "$fSection" "$fEnd"
+
+printf "%bDownloading EuroScope…\n%b" "$fStatus" "$fEnd"
+esFilename="EuroScopeSetup32.msi"
+download https://www.euroscope.hu/install/EuroScopeSetup32.msi "$esFilename"
+
+printf "%bDownloading Audio for VATSIM…\n%b" "$fStatus" "$fEnd"
+afvFilename="Audio for VATSIM.msi"
+download https://audio.vatsim.net/downloads/standalone "$afvFilename"
+
+# find link "Download latest beta" on EuroScope homepage
+printf "%bFinding link \"latest beta\" on EuroScope homepage…\n%b" "$fStatus" "$fEnd"
+download https://www.euroscope.hu/wp/ es-index.html
+_esBetaUrl=$(grep -Eo '<a.*>Download latest beta' es-index.html | grep -Eo 'http.*\.zip')
+printf "%bDownloading EuroScope beta…\n%b" "$fStatus" "$fEnd"
+esBetaFilename="EuroScope-Beta.zip"
+download "$_esBetaUrl" "$esBetaFilename"
+
+printf "%bInstalling EuroScope…\n%b" "$fSection" "$fEnd"
+wine msiexec /q /i "$esFilename"
+
+printf "%bApplying EuroScope beta…\n%b" "$fSection" "$fEnd"
+unzip -o $esBetaFilename -d drive_c/Program\ Files/EuroScope/
+
+printf "%bInstalling Audio for VATSIM…\n%b" "$fSection" "$fEnd"
+wine msiexec /i "$afvFilename"
+
+printf "%bSimulating shutdown…\n%b" "$fInfo" "$fEnd"
+wine wineboot --shutdown
+
+printf "\n%bInstallation of EuroScope and AfV is finished.\n%b" "$fSection" "$fEnd"
+printf "You can run them from your start menu (entries are in %s)\n" "$HOME/.local/share/applications/wine"
+printf "or from the command line:\n"
+
+printf "\nAudio for VATSIM:\n"
+printf "  %bWINEDEBUG=-all WINEPREFIX=$PWD wine \"$PWD/drive_c/AudioForVATSIM/AudioForVATSIM.exe\"\n%b" "$fInfo" "$fEnd"
+printf "\nEuroScope:\n"
+printf "  %bWINEDEBUG=-all WINEPREFIX=$PWD wine \"$PWD/drive_c/Program Files/EuroScope/EuroScope.exe\"\n%b" "$fInfo" "$fEnd"
+
+printf "\nUseful tools:\n"
+printf "  winecfg (wine confguration, for example: set up your Documents folder):\n"
+printf "    %bWINEARCH=win32 WINEPREFIX=$PWD winecfg\n%b" "$fInfo" "$fEnd"
+printf "  winetricks (try to solve dependency problems):\n"
+printf "    %bWINEARCH=win32 WINEPREFIX=$PWD winetricks\n%b" "$fInfo" "$fEnd"
+printf "  Stop rogue win processes:\n"
+printf "    %bWINEARCH=win32 WINEPREFIX=$PWD wine wineboot --shutdown\n%b" "$fInfo" "$fEnd"
+printf "    %bpkill wine; pkill wineserver\n%b" "$fInfo" "$fEnd"
+
+printf "\nThis is a community effort. Please report findings and improvements on https://github.com/jonaseberle/euroscope-afv-wine\n"
