ad223047d8460be2092d564fee88044b9ccc04a0
Jonas Eberle <flightvision@googlemail.com>
2022-02-02 11:28:39 +0100

    [BUGFIX] only output exit comment once

--- euroscope-afv-wine_install.sh
+++ euroscope-afv-wine_install.sh
@@ -37,7 +37,7 @@ function shutdown() {
   printf "\n%bThis is a community effort. Please report findings and improvements on https://github.com/jonaseberle/euroscope-afv-wine%b\n" \
     "$fInfo" "$fEnd"
 }
-trap shutdown EXIT INT KILL TERM
+trap shutdown EXIT
 
 function usage() {
   printf "Usage: euroscope-afv-wine_install.sh [<options>]\n"
